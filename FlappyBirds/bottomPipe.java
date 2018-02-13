418146314857021fa4dcd25d62054c820f4ddd3b
Ian Lizarda <ianliz22@icloud.com>
2018-02-12 20:55:13 -0800

    Update bottomPipe.java

--- FlappyBirds/bottomPipe.java
+++ FlappyBirds/bottomPipe.java
@@ -1,17 +1,7 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;  
 
-/**
- * Write a description of class Pipe here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class bottomPipe extends Actor
 {
-    /**
-     * Act - do whatever the Pipe wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     //method sets speed of the pipe to move to the left
     int SPEED = -4;
     public void act() 
